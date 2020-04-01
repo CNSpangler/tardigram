8b3f572b876e6f4440e6053969a347ee7e2ad8cc
Chelsea Spangler <chelseanspangler@gmail.com>
2020-04-01 13:37:48 -0700

    failing test for comments post route

--- __tests__/comments.test.js
+++ __tests__/comments.test.js
@@ -1,4 +1,4 @@
-const { getUser, getAgent, getComment, getComments } = require('../db/data-helpers');
+const { getUser, getAgent, getGram, getComment, getComments } = require('../db/data-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
@@ -6,7 +6,21 @@ const app = require('../lib/app');
 describe('comments routes', async() => {
   it('creates a new comment', async() => {
     const user = await getUser({ username: 'wario' });
+    const gram = await getGram({ user: user._id })
+
     return getAgent()
       .post('/api/v1/comments')
+      .send({
+        commentBy: user._id,
+        gram: gram._id,
+        comment: 'I know, right!'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          comment: 'I know, right!',
+          __v: 0
+        })
+      })
   }
 });
